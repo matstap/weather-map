8cebbe96e42522c61d389adf46c033c10d7ed632
Matthew Staples <staplma2@uw.edu>
2017-07-04 10:23:21 -0700

    puts the getWeather call in the if statement in calculateAndDisplayRoute function

--- map.js
+++ map.js
@@ -40,13 +40,11 @@ function calculateAndDisplayRoute(directionsService, directionsDisplay) {
     if (status === 'OK') {
       directionsDisplay.setDirections(response);
       locations = directionsDisplay.getDirections().routes[0].overview_path.filter((_, ind) => ind % 25 === 0);
-      console.log(locations.length);
       getWeather(locations, addMarkers);
       // console.log(response.routes[0].overview_path);
     } else {
       // window.alert('Directions request failed due to ' + status);
     }
-    // locations = directionsDisplay.getDirections().routes[0].overview_path.filter((_, ind) => ind % 25 === 0);
   });
 }
 
